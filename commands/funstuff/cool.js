d012f50c851a8a062051116e768a1ef009206202
Boomcrasc078 <mose.g07@gmail.com>
2024-02-16 22:22:23 +0100

    Adding cool.js

---
+++ commands/funstuff/cool.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("cool")
+    .setDescription("Replies cool, kinda cool or very cool."),
+  async execute(interaction) {
+    const random = Math.floor(Math.random() * 3);
+    switch (random) {
+      case 0:
+        await interaction.reply("You're not cool today, sorry.");
+        break;
+      case 1:
+        await interaction.reply("You're kinda cool today.");
+        break;
+      case 2:
+        await interaction.reply("You're very cool today.");
+        break;
+    }
+  },
+};
