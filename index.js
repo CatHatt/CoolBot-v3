39a2e788fc431865cfa2bc0768aeb92d2c4086e1
CatHat <liase810@gmail.com>
2024-02-19 09:30:18 +0100

    move events to separate folder

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
+console.log('Ping');
+
 // Create client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
@@ -31,42 +33,20 @@ for (const folder of commandFolders) {
     }
 }
 
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs
+    .readdirSync(eventsPath)
+    .filter((file) => file.endsWith('.js'));
 
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) {
-        console.error(
-            `No command matching ${interaction.commandName} was found.`
-        );
-        return;
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({
-                content: 'There was an error while executing this command!',
-                ephemeral: true,
-            });
-        } else {
-            await interaction.reply({
-                content: 'There was an error while executing this command!',
-                ephemeral: true,
-            });
-        }
-    }
-});
-
-// Tell console when bot us up and running
-console.log('Ping');
-
-client.once(Events.ClientReady, (readyClient) => {
-    console.log(`Pong! Logged in as ${readyClient.user.tag}`);
-});
+}
 
 // Login to bot
 client.login(token);
